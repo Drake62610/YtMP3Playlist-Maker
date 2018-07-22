5321490586c520094ca97eef344b0acdb9c9c9d7
MatteFlorian <matteflorian@live.fr>
2018-07-22 14:46:12 +0200

    requirement and small fix

--- python_youtube.py
+++ python_youtube.py
@@ -23,11 +23,11 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
 file_handler = RotatingFileHandler('activity.log', 'a', 1000000, 1)
-file_handler.setLevel(logging.DEBUG)
+file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.INFO)
+stream_handler.setLevel(logging.)
 logger.addHandler(stream_handler)
 
 # Function that will simulate a search on youtube and return the link of the top video
